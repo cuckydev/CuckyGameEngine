fc0693955f8e2c98742355db6f8a80bf11a7d344
cuckydev <cuckydev@users.noreply.github.com>
2020-05-07 06:13:24 -0400

    remove vector sample code

--- Sample.cpp
+++ Sample.cpp
@@ -34,27 +34,6 @@ int main()
 		return -1;
 	}
 	
-	CGE::Math::Vector<2, float> vec1{-5.0f, 5.0f};
-	CGE::Math::Vector<2, float> vec2{5.0f, -5.0f};
-	CGE::Math::Vector<2, float> vec3 = vec1;
-	vec3 -= vec2;
-	std::cout << vec3[0] << ", " << vec3[1] << std::endl;
-	
-	if (vec3 == CGE::Math::Vector<2, float>{0.0f, 0.0f})
-		std::cout << "I expected vec3 to equal 0, 0... looks like I was right!" << std::endl;
-	if (vec3 != CGE::Math::Vector<2, float>{0.0f, 0.0f})
-		std::cout << "I expected vec3 to equal 0, 0... looks like I was wrong!" << std::endl;
-	
-	float magnitude = vec3.Magnitude();
-	std::cout << "By the way, it has a magnitude of " << magnitude << std::endl;
-	vec3.Normalize();
-	std::cout << "But if it was 1, vec3 would be " << vec3[0] << ", " << vec3[1] << std::endl;
-	std::cout << "CRAP! I called vec3.Normalize().. how will I restore it?" << std::endl << "Right! I'll multiply it by the magnitude I had before" << std::endl;
-	vec3 *= magnitude;
-	CGE::Math::Vector<2, float> vec4 = vec3.Normalized();
-	std::cout << "Okay this time I'm gonna call Normalized, which will make a new vector that should be the same as the one before..." << std::endl << vec4[0] << ", " << vec4[1] << std::endl;
-	std::cout << "Now vec3 should be back to where it was: " << vec3[0] << ", " << vec3[1] << std::endl;
-	
 	//End CGE program
 	delete cgeInstance;
 	return 0;
