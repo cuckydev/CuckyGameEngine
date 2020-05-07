f4f675036c93e80d83aad64e17a984ed675d04f7
cuckydev <cuckydev@users.noreply.github.com>
2020-05-07 06:01:16 -0400

    vector cleanup

--- src/CGE/Math.h
+++ src/CGE/Math.h
@@ -10,7 +10,8 @@ Authors: Regan Green (cuckydev)
 */
 
 //Standard library
-#include <string>
+#include <cmath>
+#include <algorithm>
 
 //CuckyGameEngine namespace
 namespace CGE
@@ -33,90 +34,78 @@ namespace CGE
 			Vector operator+(const Vector &rhs) const
 			{
 				Vector result;
-				for (unsigned i = 0; i < dimension; i++)
-					result[i] = d[i] + rhs[i];
+				std::transform(d, d + dimension, rhs.d, result.d, std::plus<T>());
 				return result;
 			}
 			
 			Vector operator-(const Vector &rhs) const
 			{
 				Vector result;
-				for (unsigned i = 0; i < dimension; i++)
-					result[i] = d[i] - rhs[i];
+				std::transform(d, d + dimension, rhs.d, result.d, std::minus<T>());
 				return result;
 			}
 			
 			Vector operator*(const Vector &rhs) const
 			{
 				Vector result;
-				for (unsigned i = 0; i < dimension; i++)
-					result[i] = d[i] * rhs[i];
+				std::transform(d, d + dimension, rhs.d, result.d, std::multiplies<T>());
 				return result;
 			}
 			
 			Vector operator*(const T &rhs) const
 			{
 				Vector result;
-				for (unsigned i = 0; i < dimension; i++)
-					result[i] = d[i] * rhs;
+				std::transform(d, d + dimension, result.d, std::bind1st(std::multiplies<T>(), rhs));
 				return result;
 			}
 			
 			Vector operator/(const Vector &rhs) const
 			{
 				Vector result;
-				for (unsigned i = 0; i < dimension; i++)
-					result[i] = d[i] / rhs[i];
+				std::transform(d, d + dimension, rhs.d, result.d, std::divides<T>());
 				return result;
 			}
 			
 			Vector operator/(const T &rhs) const
 			{
 				Vector result;
-				for (unsigned i = 0; i < dimension; i++)
-					result[i] = d[i] / rhs;
+				std::transform(d, d + dimension, result.d, std::bind1st(std::divides<T>(), rhs));
 				return result;
 			}
 			
 			//Assignment operators
 			void operator+=(const Vector &rhs)
 			{
-				for (unsigned i = 0; i < dimension; i++)
-					d[i] += rhs[i];
+				std::transform(d, d + dimension, rhs.d, d, std::plus<T>());
 			}
 			
 			void operator-=(const Vector &rhs)
 			{
-				for (unsigned i = 0; i < dimension; i++)
-					d[i] -= rhs[i];
+				std::transform(d, d + dimension, rhs.d, d, std::minus<T>());
 			}
 			
 			void operator*=(const Vector &rhs)
 			{
-				for (unsigned i = 0; i < dimension; i++)
-					d[i] *= rhs[i];
+				std::transform(d, d + dimension, rhs.d, d, std::multiplies<T>());
 			}
 			
 			void operator*=(const T &rhs)
 			{
-				for (unsigned i = 0; i < dimension; i++)
-					d[i] *= rhs;
+				std::transform(d, d + dimension, d, std::bind1st(std::multiplies<T>(), rhs));
 			}
 			
 			void operator/=(const Vector &rhs)
 			{
-				for (unsigned i = 0; i < dimension; i++)
-					d[i] /= rhs[i];
+				std::transform(d, d + dimension, rhs.d, d, std::divides<T>());
 			}
 			
 			void operator/=(const T &rhs)
 			{
-				for (unsigned i = 0; i < dimension; i++)
-					d[i] /= rhs;
+				std::transform(d, d + dimension, d, std::bind1st(std::divides<T>(), rhs));
 			}
 			
 			//Comparision operators
-			bool operator==(const T &rhs) const
+			bool operator==(const Vector &rhs) const
 			{
 				for (unsigned i = 0; i < dimension; i++)
 				{
@@ -126,7 +115,7 @@ namespace CGE
 				return true;
 			}
 			
-			bool operator!=(const T &rhs) const
+			bool operator!=(const Vector &rhs) const
 			{
 				for (unsigned i = 0; i < dimension; i++)
 				{
@@ -159,6 +148,30 @@ namespace CGE
 					return d[0] * rhs[1] - d[0] * rhs[2]; //Scalar
 				if constexpr (dimension == 3)
 					return Vector<dimension, T>{d[1] * rhs[2] - d[2] * rhs[1], 0, 0};
+			}
+			
+			T Magnitude() const
+			{
+				T result;
+				for (const T &i : d)
+					result += i * i;
+				return std::sqrt(result);
+			}
+			
+			Vector Normalized() const
+			{
+				Vector result;
+				T magnitude = Magnitude();
+				for (unsigned i = 0; i < dimension; i++)
+					result[i] = d[i] / magnitude;
+				return result;
+			}
+			
+			void Normalize()
+			{
+				T magnitude = Magnitude();
+				for (T &i : d)
+					i /= magnitude;
 			}
 		};
 	}
